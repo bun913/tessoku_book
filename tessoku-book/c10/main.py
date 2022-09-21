2cb328a16be5fd3ba0880a42b5e4f9f6b0c854d3
bun913 <imanau03@gmail.com>
2022-09-21 21:33:36 +0900

    Initial commit

---
+++ tessoku-book/c10/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Atcoderの問題解く用
+
+全ての組み合わせを列挙する方法
+list(0...8)から2つを抜き出す
+list(combinations(l, 2))
+
+bit全探索でフラグが立っているかチェックする
+if ((i >> j) & 1)
+"""
+from functools import reduce, lru_cache
+from itertools import combinations
+import math
