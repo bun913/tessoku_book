464b459bcb7fc2f337a9819ef1ff22798dee373b
bun913 <imanau03@gmail.com>
2022-09-25 16:47:46 +0900

    feat: solve a011(user bisect)

--- tessoku-book/a11/main.py
+++ tessoku-book/a11/main.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+二部探索の練習
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from bisect import bisect_left
+
+N, X = list(map(int, input().split()))
+A = list(map(int, input().split()))
+ans = bisect_left(A, X) + 1
+print(ans)
