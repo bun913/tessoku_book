d1ae9a7a8f885aecd94efe50d2e5b995d23ffa88
bun913 <imanau03@gmail.com>
2022-09-25 16:58:04 +0900

    feat: solve a08(meguru_bisect)

--- tessoku-book/a08/main.py
+++ tessoku-book/a08/main.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+二次元配列の累積和
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from itertools import accumulate
+import numpy as np
+
+# 二次元の累積和を準備
+H, W = list(map(int, input().split()))
+X = [[0] + list(map(int, input().split(" "))) for _ in range(H)]
+X.insert(0, [0 for _ in range(W + 1)])
+accum = np.array(X)
+accum = np.cumsum(accum, axis=1)
+accum = np.cumsum(accum, axis=0)
+
+# クエリ処理
+Q = int(input())
+for _ in range(Q):
+    A, B, C, D = list(map(int, input().split()))
+    ans = accum[C][D]
+    # Aの1個左を引いて、Bの1個上を引く、さらに(A-1,B-1)の値をたす
+    ans += accum[A - 1][B - 1]
+    ans -= accum[A - 1][D]
+    ans -= accum[C][B - 1]
+    print(ans)
