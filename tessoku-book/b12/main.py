58a259223d0106b83e6fde2876c56f6c37798af0
bun913 <imanau03@gmail.com>
2022-09-27 06:44:25 +0900

    feat: solve b12

--- tessoku-book/b12/main.py
+++ tessoku-book/b12/main.py
@@ -1,14 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+正の整数Nが与えられる
+x**3 + x = Nを満たす正の実数xを出力する
+Nが10万以下であることは保証されている
+ということはxの最大値は100以下であることは保証されている
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N = int(input())
+
+
+def is_ok(x):
+    return pow(x, 3) + x >= N
+
+
+def meguru_bisect(ng, ok):
+    """
+    初期値のng,okを受け取り,is_okを満たす最小(最大)のokを返す
+    まずis_okを定義すべし
+    ng ok は  とり得る最小の値-1 とり得る最大の値+1
+    最大最小が逆の場合はよしなにひっくり返す
+    """
+    while abs(ok - ng) > 0.001:
+        mid = (ok + ng) / 2
+        if is_ok(mid):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+
+ans = meguru_bisect(-1.0, 100.0)
+print(f"{ans:.12f}")
