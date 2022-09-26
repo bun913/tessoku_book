b8580088ffd469109e47192e9300c60cd02cfa8f
bun913 <imanau03@gmail.com>
2022-09-27 06:44:17 +0900

    feat: solve a12

--- tessoku-book/a12/main.py
+++ tessoku-book/a12/main.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+N台のプリンター
+プリンターiはAi秒毎にチラシを1枚印刷
+全てのプリンターのスイッチを同時に入れた時K枚目のチラシが印刷されるのは何秒後か
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+答えが10**9を超えないということがわかっているので、答えの方で二部探索をする
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from functools import reduce
+
+N, K = list(map(int, input().split()))
+A = list(map(int, input().split()))
+
+
+def is_ok(seconds):
+    possible_cnt = reduce(lambda prev, x: prev + (seconds // x), A, 0)
+    return possible_cnt >= K
+
+
+def meguru_bisect(ng, ok):
+    while abs(ok - ng) > 1:
+        mid = (ok + ng) // 2
+        if is_ok(mid):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+
+ans = meguru_bisect(-1, 10**9 + 1)
+print(ans)
