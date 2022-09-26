14d59765c20d397974d791800dc9aafdd2a57dcd
bun913 <imanau03@gmail.com>
2022-09-26 21:08:29 +0900

    feat: solve a11

--- tessoku-book/a11/main.py
+++ tessoku-book/a11/main.py
@@ -1,10 +1,33 @@
 # -*- coding: utf-8 -*-
 """
 二部探索の練習
-"""
-from bisect import bisect_left
 
+めぐる式二部探索で実装するバージョン
+"""
 N, X = list(map(int, input().split()))
 A = list(map(int, input().split()))
-ans = bisect_left(A, X) + 1
+
+
+def is_ok(n):
+    # 条件を満たすかどうか？問題ごとに定義
+    return n > X
+
+
+def meguru_bisect(ng, ok):
+    """
+    初期値のng,okを受け取り,is_okを満たす最小(最大)のokを返す
+    まずis_okを定義すべし
+    ng ok は  とり得る最小の値-1 とり得る最大の値+1
+    最大最小が逆の場合はよしなにひっくり返す
+    """
+    while abs(ok - ng) > 1:
+        mid = (ok + ng) // 2
+        if is_ok(A[mid]):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+
+ans = meguru_bisect(-1, N)
 print(ans)
