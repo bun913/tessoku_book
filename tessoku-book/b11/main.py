1b7e94eec1a65d0f9c6298a4583fbd2724e7e4d4
bun913 <imanau03@gmail.com>
2022-09-26 21:29:40 +0900

    feat: solve b12

--- tessoku-book/b11/main.py
+++ tessoku-book/b11/main.py
@@ -1,22 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+めぐる式二部探索で実装し直すバージョン
 """
-from bisect import bisect_left
-
 N = int(input())
 A = list(map(int, input().split()))
 AD = sorted(A)
 Q = int(input())
 
+
+def is_ok(x: int, n: int):
+    # 条件を満たすかどうか？問題ごとに定義
+    return n >= x
+
+
+def meguru_bisect(x, ng, ok):
+    """
+    初期値のng,okを受け取り,is_okを満たす最小(最大)のokを返す
+    まずis_okを定義すべし
+    ng ok は  とり得る最小の値-1 とり得る最大の値+1
+    最大最小が逆の場合はよしなにひっくり返す
+    """
+    while abs(ok - ng) > 1:
+        mid = (ok + ng) // 2
+        if is_ok(x, AD[mid]):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+
 for i in range(Q):
     X = int(input())
-    ind = bisect_left(AD, X)
+    ind = meguru_bisect(X, -1, N)
     print(ind)
