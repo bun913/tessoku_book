ad6387a8a1b60e4b3440c95984d4965d542cd96d
bun913 <imanau03@gmail.com>
2022-09-28 06:11:46 +0900

    feat: solve a13(binary search)

--- tessoku-book/a13/main.py
+++ tessoku-book/a13/main.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+N個の整数
+小さい順に並んでいる。異なる二つの整数のペアを選んでその差がK以下になるような選び方
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+1つの数は総当たりで、他は二部探索で探せば良くないか
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from bisect import bisect_right
+
+N, K = list(map(int, input().split()))
+A = list(map(int, input().split()))
+
+ans = 0
+for i, a in enumerate(A):
+    j = bisect_right(A, a + K)
+    ans += j - (i + 1)
+print(ans)
