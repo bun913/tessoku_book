cc95b1569a2922d7b4f60610bea4dea437df93c1
bun913 <imanau03@gmail.com>
2022-09-30 18:56:52 +0900

    feat: update a13(syakutorihou)

--- tessoku-book/a13/main.py
+++ tessoku-book/a13/main.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-N個の整数
-小さい順に並んでいる。異なる二つの整数のペアを選んでその差がK以下になるような選び方
-
-1つの数は総当たりで、他は二部探索で探せば良くないか
+尺取り法で考え直してみた
+O(N)で解いてみる
 """
-from bisect import bisect_right
-
 N, K = list(map(int, input().split()))
 A = list(map(int, input().split()))
 
 ans = 0
-for i, a in enumerate(A):
-    j = bisect_right(A, a + K)
-    ans += j - (i + 1)
+
+# 一番左から見始める
+l = 0
+r = l + 1
+while l < N:
+    left = A[l]
+    # 左が右に追いついてしまっている場合は右をずらす
+    if l == r:
+        r += 1
+    # 右端を行けるまで右にずらす
+    while r < N and A[r] - left <= K:
+        ans += r - l
+        r += 1
+    else:
+        l += 1
 print(ans)
