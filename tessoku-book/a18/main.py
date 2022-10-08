f1e88a6f992be59c027250c0cb925043a5503549
bun913 <imanau03@gmail.com>
2022-10-09 07:22:33 +0900

    feat: solve a18

--- tessoku-book/a18/main.py
+++ tessoku-book/a18/main.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
+部分和問題
+N枚のカード
+カードの中からいくつかを選んで書かれた数の合計がS以下となる方法の確認
+Nが60なのでbit全探索で全てのパターンを試すのは無理
 
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+二次元のDPで60パターンを試していけばなんとかなりそう
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N, S = list(map(int, input().split()))
+A = list(map(int, input().split()))
+dp = [[False for _ in range(S + 1)] for _ in range(N + 1)]
+dp[0][0] = True
+
+for i in range(1, N + 1):
+    cur = A[i - 1]
+    for j in range(S + 1):
+        # 上がTrueなら特に条件なくTrue
+        if dp[i - 1][j] is True:
+            dp[i][j] = True
+        if j - cur < 0:
+            continue
+        if dp[i - 1][j - cur]:
+            dp[i][j] = True
+print("Yes" if dp[-1][-1] is True else "No")
