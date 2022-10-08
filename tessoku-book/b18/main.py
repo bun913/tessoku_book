9a5ebc88bccb202fbeffd199a46be7a4621a2bcc
bun913 <imanau03@gmail.com>
2022-10-09 07:22:42 +0900

    feat: solve b18

--- tessoku-book/b18/main.py
+++ tessoku-book/b18/main.py
@@ -1,14 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+二次元DPで部分和問題
+今回はNが60なのでbit全探索は不可
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N, S = list(map(int, input().split()))
+A = list(map(int, input().split()))
+dp = [[False for _ in range(S + 1)] for _ in range(N + 1)]
+dp[0][0] = True
+
+# DP表の作成
+ans = set()
+for i in range(1, N + 1):
+    cur = A[i - 1]
+    for j in range(S + 1):
+        if dp[i - 1][j] is True:
+            dp[i][j] = True
+            continue
+        if j >= cur and dp[i - 1][j - cur] is True:
+            dp[i][j] = True
+# Sを作れない場合
+if dp[-1][-1] is False:
+    print(-1)
+    exit()
+
+# # DPを復元する
+ans = []
+j = S
+for i in range(N, 0, -1):
+    if dp[i - 1][j] is True:
+        continue
+    j -= A[i - 1]
+    ans.append(i)
+
+print(len(ans))
+print(*ans[::-1])
