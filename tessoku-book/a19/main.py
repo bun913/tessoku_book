c63c3e5172a85989f67af3cf00df7911bca6248a
bun913 <imanau03@gmail.com>
2022-10-13 07:50:23 +0900

    feat: solve a19

--- tessoku-book/a19/main.py
+++ tessoku-book/a19/main.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+典型的なナップサック問題
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N, W = list(map(int, input().split()))
+VW = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0 for _ in range(W + 1)] for _ in range(N + 1)]
+dp[0][0] = 0
+
+for i in range(1, N + 1):
+    w = VW[i - 1][0]
+    v = VW[i - 1][1]
+    for j in range(1, W + 1):
+        # 今回のものをバックに入れられない
+        if j < w:
+            dp[i][j] = dp[i - 1][j]
+            continue
+        dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
+print(dp[-1][-1])
