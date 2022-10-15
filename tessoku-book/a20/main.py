79dba350268a5901d029f5915cc0a37da70acda9
bun913 <imanau03@gmail.com>
2022-10-15 10:46:11 +0900

    feat: solve abc20

--- tessoku-book/a20/main.py
+++ tessoku-book/a20/main.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+S = input()
+T = input()
+
+x_limit = len(S) + 1
+y_limit = len(T) + 1
+
+dp = [[0 for _ in range(y_limit)] for _ in range(x_limit)]
+# 競プロ本128pにあるように各マスでの赤い線を通れる最大値を数えていく
+for i in range(1, x_limit):
+    for j in range(1, y_limit):
+        is_same = S[i - 1] == T[j - 1]
+        if is_same is True:
+            dp[i][j] = max(dp[i - 1][j - 1] + 1, dp[i - 1][j], dp[i][j - 1])
+            continue
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+print(dp[-1][-1])
