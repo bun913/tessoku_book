b205670ee51a2962147f0cb1decc7013bec00ea3
bun913 <imanau03@gmail.com>
2022-11-02 08:27:28 +0900

    feat: solve b19

--- tessoku-book/b19/main.py
+++ tessoku-book/b19/main.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+今度はナップサックだが、Wが大きくVが小さい
+iをどの品物を使うか、jを価値とした表にする
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+N, W = list(map(int, input().split()))
+limit = 1000 * N + 1
+WV = [list(map(int, input().split())) for _ in range(N)]
+dp = [[10**15 for _ in range(limit)] for _ in range(N + 1)]
+dp[0][0] = 0
+
+for i in range(1, N + 1):
+    w = WV[i - 1][0]
+    v = WV[i - 1][1]
+    for j in range(1, limit):
+        if j < v:
+            dp[i][j] = dp[i - 1][j]
+            continue
+        dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - v] + w)
+ans = 0
+for i, w in enumerate(dp[N]):
+    if w <= W:
+        ans = i
+print(ans)
